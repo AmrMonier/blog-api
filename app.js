dd50e43ee768a14260f7a47d2b5fbb4cc3efd31c
Amr Monier <a.monier2107@gmail.com>
2021-10-02 20:15:38 +0200

    installed cookeie-parser bcrypt connect-mongo express-session

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ mongooseConnection()
     const cors = require('cors')
     const bodyParser = require('body-parser')
     const exhbs = require('express-handlebars')
+    const session = require('express-session')
+    const cookieParser = require('cookie-parser')
+    const MongoSessionStore = require('connect-mongo')
     
+    const developerRoutes = require('./routes/developerRoutes.js')
 
+    const threeHours = 3*60*60
     const app = express()
     
     app.engine('.hbs', exhbs({extname: '.hbs'}))
@@ -16,6 +21,14 @@ mongooseConnection()
     
 
     app.use(cors())
+    app.use(session({
+        secret: process.env.SESSION_KEY,
+        saveUninitialized: false,
+        cookie: {maxAge: threeHours},
+        resave: false,
+        store: MongoSessionStore.create({mongoUrl: process.env.MONGO_URI})
+    }))
+    app.use(cookieParser())
     app.use(bodyParser.urlencoded({extended: true}))
     
     app.get('/', (req, res, next) => {

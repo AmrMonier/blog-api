072330ec028abbd1ef3e30d813b8ff4ddf5d8e1d
Amr Monier <a.monier2107@gmail.com>
2021-10-04 19:14:18 +0200

    added nodemailer-express-handlebars as the emails templating engine

--- nodemailer/transporter.js
+++ nodemailer/transporter.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -7,16 +9,30 @@ const transporter = nodemailer.createTransport({
     pass: process.env.GMAIL_PASS,
   },
 });
+
+transporter.use(
+  "compile",
+  hbs({
+    viewEngine: {
+      extname: ".hbs",
+      layoutsDir: "views/email/layouts",
+      defaultLayout: "main",
+    },
+    viewPath: "views/email",
+    extName: ".hbs",
+  })
+);
+
 module.exports = {
   sendMail: (mailDetails) => {
     transporter.sendMail(mailDetails, (err, mail) => {
       if (err) {
-        console.log(err);
-        return false;
+        console.log('failed to send the email, trying again...');
+        this.sendMail(mailDetails)
       } else {
-        console.log('msg sent');
-        return true
-      };
+        console.log("msg sent");
+        return true;
+      }
     });
   },
 };
